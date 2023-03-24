ed137c9f8e1a5828e5abb375dfc302f38f568461
chris <chris.dev.edu@gmail.com>
2023-03-24 23:54:35 +0900

    lecture 04

--- lecture04/preprocess.py
+++ lecture04/preprocess.py
@@ -9,5 +9,12 @@ from tqdm import tqdm
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
-urllib.request.urlretrieve("https://raw.githubusercontent.com/e9t/nsmc/master/ratings_train.txt", filename="ratings_train.txt")
-urllib.request.urlretrieve("https://raw.githubusercontent.com/e9t/nsmc/master/ratings_test.txt", filename="ratings_test.txt")
+# 해당 링크에서 트레이닝용 ratings_train.txt 데이터와 테스트용 ratings_test.txt 데이터를 다운로드한다.
+urllib.request.urlretrieve("https://raw.githubusercontent.com/chris-dev-edu/crawling-and-NLP/main/lecture04/ratings_train.txt", filename="ratings_train.txt")
+urllib.request.urlretrieve("https://raw.githubusercontent.com/chris-dev-edu/crawling-and-NLP/main/lecture04/ratings_test.txt", filename="ratings_test.txt")
+
+train_data = pd.read_table('ratings_train.txt')
+test_data = pd.read_table('ratings_test.txt')
+
+print('훈련용 리뷰 개수 :', len(train_data))
+print(train_data[:5])
