c06c3ba166442a4909bc7a48685e98bed54ca54a
chris-dev-edu <chris.dev.edu@gmail.com>
2023-03-26 06:00:23 +0900

    lecture 04

--- lecture04/preprocess.py
+++ lecture04/preprocess.py
@@ -13,8 +13,18 @@ from tensorflow.keras.preprocessing.sequence import pad_sequences
 urllib.request.urlretrieve("https://raw.githubusercontent.com/chris-dev-edu/crawling-and-NLP/main/lecture04/ratings_train.txt", filename="ratings_train.txt")
 urllib.request.urlretrieve("https://raw.githubusercontent.com/chris-dev-edu/crawling-and-NLP/main/lecture04/ratings_test.txt", filename="ratings_test.txt")
 
+# 각 데이터를 train_data, test_data에 저장한다.
 train_data = pd.read_table('ratings_train.txt')
 test_data = pd.read_table('ratings_test.txt')
 
+# 훈련용 데이터의 리뷰 개수와 상위 5개를 출력
 print('훈련용 리뷰 개수 :', len(train_data))
 print(train_data[:5])
+
+# 테스트용 데이터의 리뷰 개수와 상위 5개를 출력
+print('테스트용 리뷰 개수 :', len(test_data))
+print(test_data[:5])
+
+# review 열의 중복 제거
+train_data.drop_duplicates(subset=['review'], inplace=True)
+print('중복 제거 후 훈련용 리뷰 개수 :', len(train_data))
