6c1d1bc47d44ded75ec9caa2697ab019073dbcea
chris-dev-edu <chris.dev.edu@gmail.com>
2023-04-02 07:56:58 +0900

    lecture 05

---
+++ lecture05/nlp.py
@@ -0,0 +1,109 @@
+import pickle
+import pandas as pd
+import numpy as np
+
+from konlpy.tag import Okt
+from tqdm import tqdm
+from keras.preprocessing.text import Tokenizer
+from keras.utils import pad_sequences
+
+train_data = pd.read_csv('ratings_train.csv')
+test_data = pd.read_csv('ratings_test.csv')
+
+train_data['document'].nunique(), train_data['label'].nunique()
+train_data.drop_duplicates(subset=['document'], inplace=True)
+train_data = train_data.dropna(how = 'any') # Null 값이 존재하는 행 제거
+train_data['document'] = train_data['document'].str.replace("[^ㄱ-ㅎㅏ-ㅣ가-힣 ]","")
+train_data['document'] = train_data['document'].str.replace('^ +', "") # white space 데이터를 empty value로 변경
+train_data['document'].replace('', np.nan, inplace=True)
+train_data = train_data.dropna(how = 'any')
+
+test_data['document'].nunique(), test_data['label'].nunique()
+test_data.drop_duplicates(subset = ['document'], inplace=True) # document 열에서 중복인 내용이 있다면 중복 제거
+test_data = test_data.dropna(how = 'any') # Null 값이 존재하는 행 제거
+test_data['document'] = test_data['document'].str.replace("[^ㄱ-ㅎㅏ-ㅣ가-힣 ]","") # 정규 표현식 수행
+test_data['document'] = test_data['document'].str.replace('^ +', "") # 공백은 empty 값으로 변경
+test_data['document'].replace('', np.nan, inplace=True) # 공백은 Null 값으로 변경
+test_data = test_data.dropna(how='any') # Null 값 제거
+
+stopwords = ['의','가','이','은','들','는','좀','잘','걍','과','도','를','으로','자','에','와','한','하다']
+okt = Okt()
+
+X_train = []
+for sentence in tqdm(train_data['document']):
+    tokenized_sentence = okt.morphs(sentence, stem=True) # 토큰화
+    stopwords_removed_sentence = [word for word in tokenized_sentence if not word in stopwords] # 불용어 제거
+    X_train.append(stopwords_removed_sentence)
+
+X_test = []
+for sentence in tqdm(test_data['document']):
+    tokenized_sentence = okt.morphs(sentence, stem=True) # 토큰화
+    stopwords_removed_sentence = [word for word in tokenized_sentence if not word in stopwords] # 불용어 제거
+    X_test.append(stopwords_removed_sentence)
+
+tokenizer = Tokenizer()
+tokenizer.fit_on_texts(X_train)
+
+threshold = 3
+total_cnt = len(tokenizer.word_index) # 단어의 수
+rare_cnt = 0 # 등장 빈도수가 threshold보다 작은 단어의 개수를 카운트
+total_freq = 0 # 훈련 데이터의 전체 단어 빈도수 총 합
+rare_freq = 0 # 등장 빈도수가 threshold보다 작은 단어의 등장 빈도수의 총 합
+
+# 단어와 빈도수의 쌍(pair)을 key와 value로 받는다.
+for key, value in tokenizer.word_counts.items():
+    total_freq = total_freq + value
+
+    # 단어의 등장 빈도수가 threshold보다 작으면
+    if(value < threshold):
+        rare_cnt = rare_cnt + 1
+        rare_freq = rare_freq + value
+
+# 전체 단어 개수 중 빈도수 2이하인 단어는 제거.
+# 0번 패딩 토큰을 고려하여 + 1
+vocab_size = total_cnt - rare_cnt + 1
+
+tokenizer = Tokenizer(vocab_size)
+tokenizer.fit_on_texts(X_train)
+X_train = tokenizer.texts_to_sequences(X_train)
+X_test = tokenizer.texts_to_sequences(X_test)
+
+y_train = np.array(train_data['label'])
+y_test = np.array(test_data['label'])
+
+drop_train = [index for index, sentence in enumerate(X_train) if len(sentence) < 1]
+X_train = np.delete(X_train, drop_train, axis=0)
+y_train = np.delete(y_train, drop_train, axis=0)
+
+max_len = 30
+X_train = pad_sequences(X_train, maxlen=max_len)
+X_test = pad_sequences(X_test, maxlen=max_len)
+
+
+from keras.layers import Embedding, Dense, LSTM
+from keras.models import Sequential
+from keras.models import load_model
+from keras.callbacks import EarlyStopping, ModelCheckpoint
+
+embedding_dim = 100
+hidden_units = 128
+
+model = Sequential()
+model.add(Embedding(vocab_size, embedding_dim))
+model.add(LSTM(hidden_units))
+model.add(Dense(1, activation='sigmoid'))
+
+es = EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=4)
+mc = ModelCheckpoint('best_model.h5', monitor='val_acc', mode='max', verbose=1, save_best_only=True)
+
+model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
+history = model.fit(X_train, y_train, epochs=15, callbacks=[es, mc], batch_size=64, validation_split=0.2)
+
+loaded_model = load_model('best_model.h5')
+print("\n 테스트 정확도: %.4f" % (loaded_model.evaluate(X_test, y_test)[1]))
+
+with open('tokenizer.pickle', 'wb') as handle:
+    pickle.dump(tokenizer, handle)
+
+with open('tokenizer.pickle', 'rb') as handle:
+    tokenizer = pickle.load(handle)
---
+++ lecture05/test.py
@@ -0,0 +1,28 @@
+import pickle
+import re
+
+from keras.utils import pad_sequences
+from konlpy.tag import Okt
+from keras.models import load_model
+
+
+okt = Okt()
+stopwords = ['의','가','이','은','들','는','좀','잘','걍','과','도','를','으로','자','에','와','한','하다']
+
+with open('tokenizer.pickle', 'rb') as handle:
+    tokenizer = pickle.load(handle)
+
+loaded_model = load_model('best_model.h5')
+def sentiment_predict(new_sentence):
+  new_sentence = re.sub(r'[^ㄱ-ㅎㅏ-ㅣ가-힣 ]','', new_sentence)
+  new_sentence = okt.morphs(new_sentence, stem=True) # 토큰화
+  new_sentence = [word for word in new_sentence if not word in stopwords] # 불용어 제거
+  encoded = tokenizer.texts_to_sequences([new_sentence]) # 정수 인코딩
+  pad_new = pad_sequences(encoded, maxlen=30) # 패딩
+  score = float(loaded_model.predict(pad_new)) # 예측
+  if(score > 0.5):
+    print("{:.2f}% 확률로 긍정 리뷰입니다.\n".format(score * 100))
+  else:
+    print("{:.2f}% 확률로 부정 리뷰입니다.\n".format((1 - score) * 100))
+
+sentiment_predict('이 영화 재미있네')
