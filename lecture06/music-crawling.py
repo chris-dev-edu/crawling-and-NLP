846fc9afaba2b8cddd0a64fd4af622bd204e3af4
chris <chris.dev.edu@gmail.com>
2023-04-08 12:18:21 +0900

    lecture 06

---
+++ lecture06/music-crawling.py
@@ -0,0 +1,54 @@
+import time
+
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Edge()
+
+def getUrlByYear(year):
+    return "https://www.genie.co.kr/chart/musicHistory?year={}&category=0".format(year)
+
+def get_song_infos(driver, titles):
+    song_links = driver.find_elements(by=By.CLASS_NAME, value="link")
+
+    num_of_songs = len(song_links)
+
+    for num in range(num_of_songs):
+        link = driver.find_elements(by=By.CLASS_NAME, value="link")[num]
+        link.find_element(by=By.TAG_NAME, value="a").click()
+
+        html = driver.page_source
+        soup = BeautifulSoup(html, 'html.parser')
+
+        info = soup.select('.value')
+        title = soup.select_one('.name').get_text().strip()
+        singer = info[0].get_text().strip()
+        play_time = info[3].get_text().strip()
+        lyrics = soup.select_one('#pLyrics').select_one('p').get_text().strip()
+
+        titles.append(title)
+
+        driver.back()
+
+all_title = {}
+
+for year in range(1970, 1980):
+    url = getUrlByYear(year)
+
+    titles = []
+
+    driver.get(url)
+    get_song_infos(driver, titles)
+
+    song_links = driver.find_elements(by=By.CLASS_NAME, value="link")
+    num_of_songs = len(song_links)
+
+    if num_of_songs > 49:
+        driver.find_element(by=By.CLASS_NAME, value='page-nav').find_elements(by=By.TAG_NAME, value='a')[1].click()
+        get_song_infos(driver, titles)
+
+    all_title[year] = titles
+    print(len(titles))
+
+print(all_title)
